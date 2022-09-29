4f60a24825e348aa7131938f15b20f9442391a16
Salchiis <d1360_audi@hotmail.com>
2022-09-29 17:07:40 -0500

    first push

---
+++ app/src/main/java/com/example/carpooling/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.carpooling;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buttonOnClick(){
+        button1 = (Button)findViewById(R.id.buttonN);
+    }
+}
