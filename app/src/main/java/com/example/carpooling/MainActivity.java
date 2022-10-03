5429457c464bfd804a4cba12eb7601e88cd9b8eb
Salchiis <d1360_audi@hotmail.com>
2022-10-03 16:17:42 -0500

    Add navigation bar

--- app/src/main/java/com/example/carpooling/MainActivity.java
+++ app/src/main/java/com/example/carpooling/MainActivity.java
@@ -1,19 +1,52 @@
 package com.example.carpooling;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.carpooling.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item ->{
+            switch (item.getItemId()){
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+
+                case R.id.activity:
+                    replaceFragment(new ActivityFragment());
+
+                    break;
+
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+
+                    break;
+
+            }
+
+
+                return  true;
+        });
     }
 
-    public void buttonOnClick(){
+    private void replaceFragment(Fragment fragment){
         //buttonN = (Button)findViewById(R.id.buttonN);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
     }
 }
