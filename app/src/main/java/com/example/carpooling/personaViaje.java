2bd0a8d48d749110b29c90daf16e072bc7b19296
Elisa <miemail@dominio.com>
2022-12-06 14:22:08 -0600

    junto

---
+++ app/src/main/java/com/example/carpooling/personaViaje.java
@@ -0,0 +1,14 @@
+package com.example.carpooling;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class personaViaje extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_persona_viaje);
+    }
+}
---
+++ app/src/main/java/com/example/carpooling/viajes_publicados.java
@@ -0,0 +1,14 @@
+package com.example.carpooling;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class viajes_publicados extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viajes_publicados);
+    }
+}
